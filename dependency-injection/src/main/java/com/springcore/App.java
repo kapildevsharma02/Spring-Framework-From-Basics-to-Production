ed58b3802164845ff6ebf61782ab0cbf5497aae1
Kapil <2270366@LTIN511242.cts.com>
2026-09-06 10:48:34 +0530

    Adding the project spring dependency injection...

---
+++ dependency-injection/src/main/java/com/springcore/App.java
@@ -0,0 +1,11 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("springconfig.xml");
+		Student s1=(Student)context.getBean("student1");
+		System.out.println(s1);
+	}
+}
---
+++ dependency-injection/src/main/java/com/springcore/Student.java
@@ -0,0 +1,39 @@
+package com.springcore;
+
+public class Student {
+
+	String name;
+	int age;
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + "]";
+	}
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+}
