9dff3457f72237149881680b7bec747198c17247
Kapil <2270366@LTIN511242.cts.com>
2026-09-06 11:15:08 +0530

    Adding the correct orientation of the files..

--- dependency-injection/src/main/java/com/springcore/App.java
+++ dependency-injection/src/main/java/com/springcore/App.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context=new ClassPathXmlApplicationContext("springconfig.xml");
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/springconfig.xml");
 		Student s1=(Student)context.getBean("student1");
 		System.out.println(s1);
 	}
