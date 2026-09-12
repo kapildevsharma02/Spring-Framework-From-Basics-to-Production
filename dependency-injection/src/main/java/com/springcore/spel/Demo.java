2672c50ba69274bc6a2f99d8741de2477922584d
Kapil <2270366@LTIN511242.cts.com>
2026-09-12 18:27:00 +0530

    Adding the more in spring expression language such as static, varible tc

--- dependency-injection/src/main/java/com/springcore/spel/Demo.java
+++ dependency-injection/src/main/java/com/springcore/spel/Demo.java
@@ -12,6 +12,39 @@ public class Demo {
 	@Value("#{2+5+56+34}")
 	private int y;
 
+	@Value("#{T(java.lang.Math).sqrt(25)}")
+	private double z;
+
+	@Value("#{T(java.lang.Math).E}")
+	private double e;
+
+	@Value("#{(new java.lang.String('Durgesh Tiwari'))}")
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getE() {
+		return e;
+	}
+
+	public void setE(double e) {
+		this.e = e;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
 	public int getX() {
 		return x;
 	}
@@ -30,7 +63,7 @@ public class Demo {
 
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + "]";
 	}
 
 }
